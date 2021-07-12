7db07417a6b0f9dcdc0fac108710c6913316570c
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 14:54:57 -0400

    fix bundle export

--- test/debug.js
+++ test/debug.js
@@ -1,4 +1,4 @@
-import '/node_modules/@javascribble/quantum/bundles/main.js';
+import '/node_modules/@javascribble/quantum/bundles/main-window.js';
 import '/bundles/main.js';
 
 document.body.style.visibility = 'visible';
